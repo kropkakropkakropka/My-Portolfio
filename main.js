4854c1c970e3049f11ddfa3c0d5d7394fa10f84a
Kuba <on.abuk@protonmail.com>
2022-06-22 20:42:00 +0200

    projects page

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const buttons = document.querySelectorAll('.tab-btn');
 const header = document.getElementById('header');
 
+// const url = `https://api.github.com/users/kropkakropkakropka/repos`;
+
+// function fetchProjectsInfo(){
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(data =>{
+//         data.forEach(repo => {
+//             const repoUrl = repo.html_url
+//             const repoName = repo.name;
+//             const languagesUrl =  repo.languages_url;
+//             fetch(languagesUrl)
+//             .then(res => res.json())
+//             .then(lan =>{
+//                 console.log(lan);
+//             })
+//         })
+//     })
+//     .catch(error=>{
+//         throw error;
+//     })
+// }
+
 buttons.forEach(button => {
     button.addEventListener('click', e =>{
         buttonTarget = button.dataset.id;
